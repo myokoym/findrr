62d5299907b0a3691a4107dc90b39071bb2a2a06
Masafumi Yokoyama <myokoym@gmail.com>
2013-12-14 01:24:21 +0900

    Add messages when an error occurred in the collect command

--- lib/findrr/command.rb
+++ lib/findrr/command.rb
@@ -5,7 +5,14 @@ module Findrr
   class Command < Thor
     desc "collect PATH", "Collect filenames (take a few minutes)"
     def collect(path)
+      begin
       Database.new.collect(path)
+      rescue => e
+        $stderr.puts <<-END_OF_MESSAGE
+Error: #{e.message}
+Hint: table schema might be changed. Please try `findrr destroy` command.
+        END_OF_MESSAGE
+      end
     end
 
     desc "search PART_OF_FILENAME", "Search for filenames in the collection"
